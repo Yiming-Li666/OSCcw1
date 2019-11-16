02f9adf0e47438ed3a0a6b0c25768c89d81b9c07
Yiming Li <1870274845@qq.com>
2019-11-16 22:59:13 +0800

    First runnable program for Question 1.

---
+++ cw1/ChildP1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int ChildP1(int n) {
+	n = n * 2;
+	return n;
+}
+
+// int main(int argc, char const *argv[])
+// {
+// 	ChildP1();
+// 	printf("End\n");
+// }
---
+++ cw1/ChildP2.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int ChildP2(int n) {
+	n = n - 10;
+	return n;
+}
